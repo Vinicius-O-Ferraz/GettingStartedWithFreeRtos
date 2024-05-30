9cff5dbd554393058ea100d92e0b9a48900485a6
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-05-30 10:30:44 -0300

    feat: queue implementation

---
+++ Rtos_HelloWorld/src/main.cpp
@@ -0,0 +1,65 @@
+#include <Arduino.h>  // Inclui a biblioteca principal do Arduino
+#include <Arduino.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+// Verifica se o FreeRTOS está configurado para usar um núcleo
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;  // Define o núcleo de aplicação como 0
+#else
+static const BaseType_t app_cpu = 1;  // Define o núcleo de aplicação como 1
+#endif
+
+//define queue length
+static const uint8_t msg_queue_length = 5;
+static QueueHandle_t msg_queue;
+
+// Handles para as tarefas
+static TaskHandle_t task_1 = NULL;
+static TaskHandle_t task_2 = NULL;
+static TaskHandle_t task_3 = NULL;
+
+// Função da tarefa 1
+void task(void *parameter){
+
+    int item;
+
+    while(1){
+        if(xQueueReceive(msg_queue, (void *)&item, 0)==pdTRUE){
+            Serial.println(item);
+        }
+    vTaskDelay(500/ portTICK_PERIOD_MS);
+    }
+}
+
+void setup(){
+    Serial.begin(115200);
+    vTaskDelay(1000/ portTICK_PERIOD_MS);
+    Serial.println();
+    Serial.println("--FreeRtos Queue Demo--");
+
+    msg_queue = xQueueCreate(msg_queue_length,sizeof(int));
+
+    xTaskCreatePinnedToCore(
+        task,
+        "task",
+        1024,
+        NULL,
+        1,
+        NULL,
+        app_cpu);
+}
+
+void loop(){
+    static int num = 0;
+
+    if (xQueueSend(msg_queue, (void *)&num, 10) != pdTRUE){
+        Serial.println("Queue full");
+    }
+    num++;
+    vTaskDelay(1000/ portTICK_PERIOD_MS);
+
+}
--- Rtos_HelloWorld/tests/bme.cpp
+++ Rtos_HelloWorld/tests/bme.cpp
@@ -1,96 +1,118 @@
-
-/***************************************************************************
-  This is a library for the BME280 humidity, temperature & pressure sensor
-
-  Designed specifically to work with the Adafruit BME280 Breakout
-  ----> http://www.adafruit.com/products/2650
-
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface. The device's I2C address is either 0x76 or 0x77.
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
-  See the LICENSE file for details.
- ***************************************************************************/
+#include <Arduino.h>  // Inclui a biblioteca principal do Arduino
+#include <Arduino.h>
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
+// Verifica se o FreeRTOS está configurado para usar um núcleo
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu = 0;  // Define o núcleo de aplicação como 0
+#else
+static const BaseType_t app_cpu = 1;  // Define o núcleo de aplicação como 1
+#endif
 
-#define SEALEVELPRESSURE_HPA (1013.25)
+//define queue length
+static const uint8_t msg_queue_length = 32;
+static QueueHandle_t msg_queue;
 
-Adafruit_BME280 bme; // I2C
-//Adafruit_BME280 bme(BME_CS); // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK); // software SPI
+// Handles para as tarefas
+static TaskHandle_t task_1 = NULL;
+static TaskHandle_t task_2 = NULL;
+static TaskHandle_t task_3 = NULL;
 
-unsigned long delayTime;
+// Função da tarefa 1
+void bme(void *parameter){
 
-TwoWire i2cbme = TwoWire(0);
+    while(1){
+        Serial.println();  // Imprime uma linha em branco
+            Serial.print("Temperature = ");
+            //Serial.print(bme.readTemperature());
+            Serial.println(" °C");
 
-void printValues() {
-    Serial.print("Temperature = ");
-    Serial.print(bme.readTemperature());
-    Serial.println(" °C");
+            Serial.print("Pressure = ");
+            //Serial.print(bme.readPressure() / 100.0F);
+            Serial.println(" hPa");
 
-    Serial.print("Pressure = ");
+            Serial.print("Approx. Altitude = ");
+            //Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+            Serial.println(" m");
 
-    Serial.print(bme.readPressure() / 100.0F);
-    Serial.println(" hPa");
-
-    Serial.print("Approx. Altitude = ");
-    Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-    Serial.println(" m");
-
-    Serial.print("Humidity = ");
-    Serial.print(bme.readHumidity());
-    Serial.println(" %");
-
-    Serial.println();
-}
-
-void setup() {
-    i2cbme.begin(21,22,100000);
-    Serial.begin(9600);
-    while(!Serial);    // time to get serial running
-    Serial.println(F("BME280 test"));
-
-    unsigned status;
-    
-    // default settings
-    //status = bme.begin();
-
-    // You can also pass in a Wire library object like &Wire2
-    status = bme.begin(0x76,&i2cbme);
-    if (!status) {
-        Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-        Serial.print("SensorID was: 0x"); Serial.println(bme.sensorID(),16);
-        Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-        Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-        Serial.print("        ID of 0x60 represents a BME 280.\n");
-        Serial.print("        ID of 0x61 represents a BME 680.\n");
-        while (1) delay(10);
+            Serial.print("Humidity = ");
+            //Serial.print(bme.readHumidity());
+            Serial.println(" %");
+                }
+        Serial.println();  // Imprime uma nova linha
+        vTaskDelay(6000 / portTICK_PERIOD_MS);  // Espera 1 segundo
     }
-    
-    Serial.println("-- Default Test --");
-    delayTime = 1000;
 
+// Função da tarefa 2
+void anem(void *parameter){
+    while(1){
+        //Sample++;
+        //Serial.print(Sample);
+        Serial.print("Start measurement...");
+        //windvelocity();
+        Serial.println("   finished.");
+        Serial.print("Counter: ");
+        //Serial.print(counter);
+        Serial.println("RPM: ");
+        //RPMcalc();
+        //Serial.print(RPM);
+        Serial.println("Wind speed: ");
+
+        Serial.println();  // Imprime uma nova linha
+        vTaskDelay(6000 / portTICK_PERIOD_MS);  // Espera 1 segundo
+    }
+}
+
+void connection(void *parameter){
     Serial.println();
+    Serial.println("Connecting to server via mqts");
+    for (int i =0; i > 4; i++){
+            delay(100);
+            Serial.print(".");
+    }
 }
 
+void setup(){
+    Serial.begin(300);  // Inicializa a comunicação serial com baud rate de 300
 
-void loop() { 
-    printValues();
-    delay(delayTime);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);  // Espera 1 segundo
+    Serial.println();    
+    Serial.print("Setup and loop task running on core ");
+    Serial.print(xPortGetCoreID());  // Imprime o núcleo no qual a função setup está rodando
+    Serial.print(" with priority ");
+    Serial.println(uxTaskPriorityGet(NULL));  // Imprime a prioridade da tarefa atual
+
+    // Cria a tarefa 1 e a fixa no núcleo especificado
+    xTaskCreatePinnedToCore(bme,
+                            "Task 1",  // Nome da tarefa
+                            1024,  // Tamanho da pilha
+                            NULL,  // Parâmetro para a tarefa
+                            2,  // Prioridade da tarefa
+                            &task_1,  // Handle da tarefa
+                            app_cpu);  // Núcleo da CPU
+
+    // Cria a tarefa 2 e a fixa no núcleo especificado
+    xTaskCreatePinnedToCore(anem,
+                            "Task 2",  // Nome da tarefa
+                            1024,  // Tamanho da pilha
+                            NULL,  // Parâmetro para a tarefa
+                            1,  // Prioridade da tarefa
+                            &task_2,  // Handle da tarefa
+                            app_cpu);  // Núcleo da CPU
+
+    xTaskCreatePinnedToCore(connection,
+                            "Task 3",  // Nome da tarefa
+                            1024,  // Tamanho da pilha
+                            NULL,  // Parâmetro para a tarefa
+                            1,  // Prioridade da tarefa
+                            &task_3,  // Handle da tarefa
+                            app_cpu);  // Núcleo da CPU
 }
 
-
+void loop(){
+ 
+}
